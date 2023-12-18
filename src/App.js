42d7a1140da7632d2ef8707d7d163f5f9e488945
Dilawar Shehzad (DS) <86353151+Dilawar-Shehzad@users.noreply.github.com>
2023-12-18 10:20:26 -0800

    Update App.js
    new temp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
-// All pages
+// All pages for Investor 
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import DemoProduct from './pages/DemoProduct';
